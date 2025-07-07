3eae419fd16fffdbf1d4b6d3854b6bf81258d408
Dubby0610 <>
2025-07-07 08:01:20 -0400

    fix: cors

--- src/app.js
+++ src/app.js
@@ -10,33 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    // origin: [
-      // "*"
-      // "http://localhost:5173/",
-      // "https://groove-kappa.vercel.app/"
-    // ],
-    // credentials: true,
-    // methods: [
-    //   "GET",
-    //   "HEAD",
-    //   "PUT",
-    //   "PATCH",
-    //   "POST",
-    //   "DELETE",
-    //   "OPTIONS"
-    // ],
-    // allowedHeaders: [
-    //   "Content-Type",
-    //   "Authorization",
-    //   "X-Requested-With",
-    //   "Accept",
-    //   "Origin"
-    // ],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
@@ -47,8 +21,18 @@ app.use("/payment", paymentRoutes);
 
 // Error handler
 app.use((err, req, res, next) => {
+
+  res.header("Access-Control-Allow-Origin", "*"); // Or your frontend URL
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+
   console.error(err.stack);
   res.status(500).json({ error: "Internal Server Error" });
+  
 });
 
 export default app;
